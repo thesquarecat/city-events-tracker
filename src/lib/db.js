46ba1120e273c5a8d4ee1a7c63c2f08b18c1eb9d
thesquarecat <trungchiduong2000@gmail.com>
2026-04-27 15:43:41 -0700

    Use environment variables for DB config

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,9 +3,9 @@ import pg from "pg";
 const { Pool } = pg;
 
 export const pool = new Pool({
-    user: "cityapp",
-    host: "localhost",
-    database: "cse412_midterm",
-    password: "citypass",
-    port: 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT || 5432),
 });
